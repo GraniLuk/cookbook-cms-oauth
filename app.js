e8430ad326d6a4d0d879d157d638f360dc98b3eb
GraniLuk <granica.lukasz@gmail.com>
2025-11-30 12:53:05 +0100

    fix: add /api prefix to routes and fix module loading

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 require('dotenv').config({ silent: true })
-const express = require('express')
 const middleWarez = require('./index.js')
-const port = process.env.PORT || 3000
+const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use((req, res, next) => {
-  console.log('Request URL:', req.originalUrl);
-  next();
-});
+  console.log('Request URL:', req.originalUrl)
+  next()
+})
 
-app.get('/test', (req, res) => {
-  res.send('Test route is working!');
-});
+app.get('/api/test', (req, res) => {
+  res.send('Test route is working!')
+})
 
 // Initial page redirecting to Github
-app.get('/auth', middleWarez.auth)
+app.get('/api/auth', middleWarez.auth)
 
 // Callback service parsing the authorization token
 // and asking for the access token
-app.get('/callback', middleWarez.callback)
+app.get('/api/callback', middleWarez.callback)
 
-app.get('/success', middleWarez.success)
+app.get('/api/success', middleWarez.success)
 app.get('/', middleWarez.index)
 
 app.listen(port, () => {
-  console.log("Netlify CMS OAuth provider listening on port " + port)
+  console.log(`Netlify CMS OAuth provider listening on port ${port}`)
 })
